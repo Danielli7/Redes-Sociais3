63962b84cf429c69518e4607cdd9a61c6a6ac280
Danielli7 <148447477+Danielli7@users.noreply.github.com>
2024-10-23 13:53:27 -0300

    j

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -1,2 +1,7 @@
 const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
-async function visualizarInformacoesGlobais() 
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
+    const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
+    const horas = parseInt(dados.tempo_medio)
